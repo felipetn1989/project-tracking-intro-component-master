461a33cbb7903e16f66ee8ec7c49697784d868fa
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-12 08:50:14 -0300

    started desktop version

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
-let info = document.querySelector('.info')
+let info = document.querySelector(".info");
 
 hamburger_menu.addEventListener("click", () => {
-    if (window.getComputedStyle(nav__menu).display == 'none') {
-        nav__menu.style.display = 'grid'
-        info.style.marginTop = '3.625rem'
-    } else {
-        nav__menu.style.display = 'none'
-        info.style.marginTop = "20.5625rem";
-    }
-})
+  if (window.getComputedStyle(nav__menu).display == "none") {
+    nav__menu.style.display = "grid";
+    info.style.marginTop = "3.625rem";
+  } else {
+    nav__menu.style.display = "none";
+    info.style.marginTop = "20.5625rem";
+  }
+});
