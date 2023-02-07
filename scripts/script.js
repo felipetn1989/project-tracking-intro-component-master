9caf748e62a67d3084ac091fc1ec7405b88f5db7
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-02-07 09:21:16 -0300

    code update

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,8 @@
 let info = document.querySelector(".info");
 
-
-/* I used window.getComputedStyle().display to avoid the problem of the menu appearing for the first time only if I clicked on it twice (because the code can't access the current value of the display property unless I use getComputed style */
+/* Added a function to toggle the appropriate classes when we click on the menu icon */
 
 hamburger_menu.addEventListener("click", () => {
-  if (window.getComputedStyle(nav__menu).display == "none") {
-    nav__menu.style.display = "grid";
-    info.style.marginTop = "3.625rem";
-  } else {
-    nav__menu.style.display = "none";
-    info.style.marginTop = "20.5625rem";
-  }
+  nav__menu.classList.toggle("display");
+  info.classList.toggle("margin");
 });
